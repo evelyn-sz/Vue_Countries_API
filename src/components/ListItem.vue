8ed9642bcb3c83e436fe1c58505246f1ba4500b6
evelyn-sz <evelynszarek@gmail.com>
2021-01-13 17:03:25 +0000

    MVP completed lol

---
+++ src/components/ListItem.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+    <div>
+        <li v-on:click="handleClick"> {{ country.name }} </li>
+    </div>
+  
+</template>
+
+<script>
+
+import { eventBus } from '../main.js';
+
+export default {
+    name: 'list-item',
+    props: ['country'],
+    methods: {
+        handleClick: function(){
+            // console.log('country', this.country)
+            eventBus.$emit('country-selected', this.country);
+        }
+        
+    }
+
+}
+</script>
+
+<style>
+
+</style>
